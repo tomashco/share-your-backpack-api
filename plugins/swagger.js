0bdb09e4cf65461a1f027c0429f9f0cc9ad0367a
tmaioli <tmaioli@deloitte.it>
2023-09-07 13:21:11 +0200

    chore: update swagger and cleaned up project

--- plugins/swagger.js
+++ plugins/swagger.js
@@ -2,11 +2,12 @@
 
 const fp = require('fastify-plugin')
 const fastifySwagger = require('@fastify/swagger')
+const fastifySwaggerUi = require('@fastify/swagger-ui')
+
 const pkg = require('../package.json')
 
 module.exports = fp(async function swaggerPlugin (fastify, opts) {
   fastify.register(fastifySwagger, {
-    routePrefix: '/docs',
     exposeRoute: fastify.secrets.NODE_ENV !== 'production',
     swagger: {
       info: {
@@ -15,5 +16,20 @@ module.exports = fp(async function swaggerPlugin (fastify, opts) {
         version: pkg.version
       }
     }
+  })
+
+  fastify.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+    initOAuth: { },
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) { next() },
+      preHandler: function (request, reply, next) { next() }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header
   })
 }, { dependencies: ['application-config'] })
--- routes/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = async function root (fastify, opts) {
-  fastify.get('/', async function welcomeHandler (request, reply) {
-    return { root: true }
-  })
-}
